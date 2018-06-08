e1e2dff159ba0e3bfe524751d9b98f2a2407915d
Diwakar Sharma <diwakar1@umbc.edu>
2018-06-08 14:45:09 -0400

    Bonus version of FizzBuzz - I've got this!".

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,75 +1,91 @@
 package com.company;
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Random;
 public class Main {
+
+    static int ind = 0;
+    static String[] a = {"rock", "Paper", "scissors","Lizard","spock"};
+
 
     public static void main(String[] args) {
         Random rand = new Random();
         int random1 = getRandomNumber();
         int random2 = getRandomNumber();
 
+        System.out.println("welcome to the Crazy Fun Insane FizBuzz");
+
         if (random1 == random2) {
             System.out.println("Shazam!!");
-        }
-        else {
-                if (random1 < random2) {
-                    int stor = random1;
-                    random1 = random2;
-                    random2 = stor;
-                }
-                if ((random1 - random2) < 10) {
-                    System.out.println("this won't take long");
+        } else {
+            if (random1 < random2) {
+                int stor = random1;
+                random1 = random2;
+                random2 = stor;
+            }
+            if ((random1 - random2) < 10) {
+                System.out.println("this won't take long");
 
-                }
+            }
 
-                for (int i = random2; i <= random1; i++) {
-                    String Fizz = getBuzz(i);
-                    System.out.println(Fizz);
+            for (int i = random2; i <= random1; i++) {
+                String Fizz = getBuzz(i);
+                System.out.println(Fizz);
 
-                }
+            }
         }
     }
 
 
-        public static String getBuzz ( int i){
+    public static String getBuzz(int i) {
 
-            String fb ;
-
-
+        String fb = "";
 
 
-            if ((i % 3 == 0) && (i % 5 == 0)) {
-                fb = "FizzBuzz";
-            } else if ((i % 3 == 0)) {
-                fb = "Fizz";
-            } else if (i % 5 == 0) {
-                fb = "Buzz";
-            } else {
-                fb = Integer.toString(i);
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            fb = "FizzBuzz";
+        } else if ((i % 3 == 0)) {
+            fb = "Fizz";
+        } else if (i % 5 == 0) {
+            fb = "Buzz";
+
+
+        } else {
+            fb = Integer.toString(i);
+
+        }
+        if (i % 7 == 0) {
+
+            fb = fb + " -This number is multiple of seven";
+
+        }
+        else if ((i % 2 == 0) && (i > 10)) {
+
+            fb = fb + " " + a[ind];
+            ind++;
+            if(ind >4 ){
+
+                ind =0;
 
             }
-            if (i%7 == 0){
-
-                fb = fb+ " -This number is multiple of seven";
-
-            }
 
 
+        }
+        return fb;
+    }
 
-            return fb;
 
+        public static int getRandomNumber () {
+            Random rand = new Random();
+
+            int num = rand.nextInt(100) + 1;
+
+            return num;
         }
 
 
- public static int getRandomNumber(){
-     Random rand = new Random();
-
-        int num = rand.nextInt(20)+1;
-
-        return num;
- }
-
-
     }
+
 
 
 
